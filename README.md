8cf37818ca7b5cebbedcf39fae13d8f1285d0e3e
crypto <bathingasian001@gmail.com>
2023-04-09 20:54:02 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Monthly Report For GMO-BOT and OANDA-BOT
+
+## What is this?
+
+GMOとOANDAのボットの月末報告スクリプトです。
+実行時点で、翌日が「1」日であれば、取引成績等を収集しツイートします。
+
+以下の画像を投稿します。
+
+### GMO
+
+- バックテストと実取引の比較グラフ
+- 月次取引成績
+
+[*sgcheck*](https://github.com/zenryokukun/sgcheck)を実行します。
+
+### OANDA
+
+- バックテストと実取引の比較グラフ
+
+[*oanda-backtest*](https://github.com/zenryokukun/oanda-backtest)を実行します。
+
+## How should I use this?
+
+### 前提
+
+- *sgcheck*が~/goproject/sgcheckで稼働していること
+- *oanda-backtest*が~/goproject/oanda-botで稼働していること
+
+### 利用方法
+
+**report.sh**をcronで毎晩23:50分くらいに1回実行するよう登録してください。
+
---
+++ report.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#########################################################
+# Monthly Report for GMO-BOT and OANDA-BOT.
+# Run this script everyday, around 23PM.
+# It will summarize the report if it's end of month.
+# If not, it does nothing.
+#########################################################
+
+# tomorrow's day, in 1~31. If `nday` is 1, then
+# today is the last day of the month.
+nday=`date -d "1 day" +%d`
+
+# if not end of month, exit.
+if [ $nday -ne 1 ]; then
+	echo Not end of month. Exit...
+	exit 0
+fi
+
+##########################################################
+# end of month
+##########################################################
+
+# execute `sgcheck`
+# `sgcheck` is a comparison and montly report for GMO-BOT.
+cd ~/goproject/sgcheck
+./sgcheck "compare"
+
+# execute `oanda-backtest`
+# `oanda-backtest` is a comparison for OANDA-BOT.
+cd ~/goproject/oanda-backtest
+./oanda-backtest
+
+echo END
+exit 0
